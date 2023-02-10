f90dfb8da7e9cee2b824b2aec549d60ef3673b15
Dr Sarin CR M.Tech MBA Ph.D CBA <sarincrg@gmail.com>
2023-02-10 16:15:54 +0530

    Base

---
+++ 03.Random.py
@@ -0,0 +1,6 @@
+import torch
+import numpy as np
+x = torch.randn(10, 3)
+y = torch.randn(10, 2)
+print(x)
+print(y)
---
+++ 04.Add.py
@@ -0,0 +1,7 @@
+import torch
+import numpy as np
+x = torch.randn(2, 2)
+y = torch.randn(2, 2)
+print(x,y)
+print(x+y)
+ 
